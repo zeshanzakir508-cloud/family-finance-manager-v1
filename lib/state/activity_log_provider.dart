4f9cd379ab1479370fcea9594921df440386f65a
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-17 23:20:17 +0500

    Update activity_log_provider.dart

--- lib/state/activity_log_provider.dart
+++ lib/state/activity_log_provider.dart
@@ -1,1 +1,41 @@
+import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/activity_log_model.dart';
+
+final activityLogProvider = StateNotifierProvider<ActivityLogNotifier,
+    AsyncValue<List<ActivityLogModel>>>(
+  (ref) => ActivityLogNotifier(),
+);
+
+class ActivityLogNotifier
+    extends StateNotifier<AsyncValue<List<ActivityLogModel>>> {
+  ActivityLogNotifier() : super(const AsyncValue.loading());
+
+  StreamSubscription<List<ActivityLogModel>>? _subscription;
+
+  Future<void> loadActivityLogs(String familyId) async {
+    state = const AsyncValue.loading();
+
+    // TODO:
+    // Listen to activity logs for the family.
+  }
+
+  Future<void> refresh() async {
+    // TODO:
+    // Reload activity logs.
+  }
+
+  Future<void> clear() async {
+    await _subscription?.cancel();
+    _subscription = null;
+    state = const AsyncValue.data([]);
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+}

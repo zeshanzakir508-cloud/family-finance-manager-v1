b0cbc4bde0676df48ade4b5f0f8547605787f2b2
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-17 23:21:57 +0500

    Update settings_provider.dart

--- lib/state/settings_provider.dart
+++ lib/state/settings_provider.dart
@@ -1,1 +1,36 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../models/app_settings_model.dart';
+
+final settingsProvider =
+    StateNotifierProvider<SettingsNotifier, AsyncValue<AppSettingsModel?>>(
+  (ref) => SettingsNotifier(),
+);
+
+class SettingsNotifier
+    extends StateNotifier<AsyncValue<AppSettingsModel?>> {
+  SettingsNotifier() : super(const AsyncValue.loading());
+
+  Future<void> loadSettings(String familyId) async {
+    state = const AsyncValue.loading();
+
+    // TODO:
+    // Load app settings from Firestore repository.
+  }
+
+  Future<void> refresh() async {
+    // TODO:
+    // Reload settings.
+  }
+
+  Future<void> clear() async {
+    state = const AsyncValue.data(null);
+  }
+
+  Future<void> updateSettings(AppSettingsModel settings) async {
+    state = AsyncValue.data(settings);
+
+    // TODO:
+    // Save settings to Firestore repository.
+  }
+}

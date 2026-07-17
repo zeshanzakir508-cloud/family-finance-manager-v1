855aa1e6a268e1de5f6af9d856229c0d6b539982
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-17 23:16:30 +0500

    Update family_provider.dart

--- lib/state/family_provider.dart
+++ lib/state/family_provider.dart
@@ -1,1 +1,40 @@
+import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/family_model.dart';
+
+final familyProvider =
+    StateNotifierProvider<FamilyNotifier, AsyncValue<FamilyModel?>>(
+  (ref) => FamilyNotifier(),
+);
+
+class FamilyNotifier extends StateNotifier<AsyncValue<FamilyModel?>> {
+  FamilyNotifier() : super(const AsyncValue.loading());
+
+  StreamSubscription<FamilyModel?>? _subscription;
+
+  Future<void> loadFamily(String familyId) async {
+    state = const AsyncValue.loading();
+
+    // TODO:
+    // Listen to the family document from Firestore repository.
+  }
+
+  Future<void> refresh() async {
+    // TODO:
+    // Reload current family.
+  }
+
+  Future<void> clear() async {
+    await _subscription?.cancel();
+    _subscription = null;
+    state = const AsyncValue.data(null);
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+}

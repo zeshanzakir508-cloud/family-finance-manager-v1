0110c386d64f6727be08e34f96eab0566ab207d0
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-17 23:22:37 +0500

    Update theme_provider.dart

--- lib/state/theme_provider.dart
+++ lib/state/theme_provider.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final themeProvider =
+    StateNotifierProvider<ThemeNotifier, ThemeMode>(
+  (ref) => ThemeNotifier(),
+);
+
+class ThemeNotifier extends StateNotifier<ThemeMode> {
+  ThemeNotifier() : super(ThemeMode.system);
+
+  void setTheme(ThemeMode themeMode) {
+    state = themeMode;
+
+    // TODO:
+    // Persist theme preference.
+  }
+
+  void setLightTheme() {
+    state = ThemeMode.light;
+
+    // TODO:
+    // Persist theme preference.
+  }
+
+  void setDarkTheme() {
+    state = ThemeMode.dark;
+
+    // TODO:
+    // Persist theme preference.
+  }
+
+  void setSystemTheme() {
+    state = ThemeMode.system;
+
+    // TODO:
+    // Persist theme preference.
+  }
+
+  Future<void> loadTheme() async {
+    // TODO:
+    // Load theme preference from local storage.
+  }
+}

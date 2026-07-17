735e0ec0d9a0d44e1cbacfa43a37530cbbc130c1
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-17 23:19:37 +0500

    Update notification_provider.dart

--- lib/state/notification_provider.dart
+++ lib/state/notification_provider.dart
@@ -1,1 +1,41 @@
+import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/notification_model.dart';
+
+final notificationProvider = StateNotifierProvider<
+    NotificationNotifier, AsyncValue<List<NotificationModel>>>(
+  (ref) => NotificationNotifier(),
+);
+
+class NotificationNotifier
+    extends StateNotifier<AsyncValue<List<NotificationModel>>> {
+  NotificationNotifier() : super(const AsyncValue.loading());
+
+  StreamSubscription<List<NotificationModel>>? _subscription;
+
+  Future<void> loadNotifications(String userId) async {
+    state = const AsyncValue.loading();
+
+    // TODO:
+    // Listen to all notifications belonging to the user.
+  }
+
+  Future<void> refresh() async {
+    // TODO:
+    // Reload notification list.
+  }
+
+  Future<void> clear() async {
+    await _subscription?.cancel();
+    _subscription = null;
+    state = const AsyncValue.data([]);
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+}

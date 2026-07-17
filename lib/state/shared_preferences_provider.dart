fadd91a5371e89c888f635a6477e2dc3adc617dd
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-17 23:13:22 +0500

    Update shared_preferences_provider.dart

--- lib/state/shared_preferences_provider.dart
+++ lib/state/shared_preferences_provider.dart
@@ -1,1 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+final sharedPreferencesProvider =
+    FutureProvider<SharedPreferences>((ref) async {
+  return SharedPreferences.getInstance();
+});

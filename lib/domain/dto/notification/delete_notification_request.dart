22e2e6bef26e7fb605af72c57680afd57f22b83d
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:42:22 +0500

    Update delete_notification_request.dart

--- lib/domain/dto/notification/delete_notification_request.dart
+++ lib/domain/dto/notification/delete_notification_request.dart
@@ -1,1 +1,8 @@
+/// Request object for deleting a notification.
+class DeleteNotificationRequest {
+  final String notificationId;
 
+  const DeleteNotificationRequest({
+    required this.notificationId,
+  });
+}

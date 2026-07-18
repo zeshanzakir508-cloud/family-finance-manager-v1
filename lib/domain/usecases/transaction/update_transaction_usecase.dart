b2904e83930b15a2a4ce6e71a4af693c7b9799a7
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 14:58:37 +0500

    Update update_transaction_usecase.dart

--- lib/domain/usecases/transaction/update_transaction_usecase.dart
+++ lib/domain/usecases/transaction/update_transaction_usecase.dart
@@ -1,1 +1,36 @@
+import '../../../repositories/transaction_repository.dart';
+import '../../dto/transaction/update_transaction_request.dart';
+import '../../exceptions/not_found_exception.dart';
+import '../../usecases/base_usecase.dart';
+import '../../validators/transaction_validator.dart';
 
+class UpdateTransactionUseCase
+    implements UseCase<void, UpdateTransactionRequest> {
+  final TransactionRepository _repository;
+  final TransactionValidator _validator;
+
+  const UpdateTransactionUseCase({
+    required TransactionRepository repository,
+    required TransactionValidator validator,
+  })  : _repository = repository,
+        _validator = validator;
+
+  @override
+  Future<void> call(UpdateTransactionRequest params) async {
+    _validator.validate(params);
+
+    final exists = await _repository.transactionExists(
+      params.transaction.id,
+    );
+
+    if (!exists) {
+      throw const NotFoundException(
+        'Transaction not found.',
+      );
+    }
+
+    await _repository.updateTransaction(
+      params.transaction,
+    );
+  }
+}

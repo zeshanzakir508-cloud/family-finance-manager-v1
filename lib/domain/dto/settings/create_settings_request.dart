7e4995c3255eaf308b29b40d8a5c9cb08988c6d3
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 19:33:11 +0500

    Update create_settings_request.dart

--- lib/domain/dto/settings/create_settings_request.dart
+++ lib/domain/dto/settings/create_settings_request.dart
@@ -1,1 +1,16 @@
+import '../../../models/app_settings_model.dart';
 
+/// ============================================================================
+/// Family Finance Manager
+/// Create Settings Request
+/// ----------------------------------------------------------------------------
+/// Request object used when creating application settings.
+/// ============================================================================
+class CreateSettingsRequest {
+  /// Application settings.
+  final AppSettingsModel settings;
+
+  const CreateSettingsRequest({
+    required this.settings,
+  });
+}

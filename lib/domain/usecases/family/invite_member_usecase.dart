702023daf78db6bc75f6a5e41991f66fac502c55
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:21:45 +0500

    Update invite_member_usecase.dart

--- lib/domain/usecases/family/invite_member_usecase.dart
+++ lib/domain/usecases/family/invite_member_usecase.dart
@@ -1,1 +1,22 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/invite_member_request.dart';
+import '../../usecases/base_usecase.dart';
 
+class InviteMemberUseCase
+    implements UseCase<void, InviteMemberRequest> {
+  final FamilyRepository _repository;
+
+  const InviteMemberUseCase({
+    required FamilyRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    InviteMemberRequest params,
+  ) async {
+    await _repository.inviteMember(
+      familyId: params.familyId,
+      email: params.email,
+    );
+  }
+}

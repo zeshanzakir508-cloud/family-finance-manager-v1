b6dd08bdb0728f9fa64676556534fddbbbedb589
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:08:09 +0500

    Update restore_account_usecase.dart

--- lib/domain/usecases/account/restore_account_usecase.dart
+++ lib/domain/usecases/account/restore_account_usecase.dart
@@ -1,1 +1,23 @@
+import '../../../repositories/account_repository.dart';
+import '../../exceptions/not_found_exception.dart';
+import '../../usecases/base_usecase.dart';
 
+class RestoreAccountUseCase
+    implements UseCase<void, String> {
+  final AccountRepository _repository;
+
+  const RestoreAccountUseCase({
+    required AccountRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(String accountId) async {
+    final exists = await _repository.accountExists(accountId);
+
+    if (!exists) {
+      throw const NotFoundException('Account not found.');
+    }
+
+    await _repository.restoreAccount(accountId);
+  }
+}

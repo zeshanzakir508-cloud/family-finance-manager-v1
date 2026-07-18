6d106ab3cb36905d846e8138de75d3b02de4403e
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:22:08 +0500

    Update remove_member_usecase.dart

--- lib/domain/usecases/family/remove_member_usecase.dart
+++ lib/domain/usecases/family/remove_member_usecase.dart
@@ -1,1 +1,22 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/remove_member_request.dart';
+import '../../usecases/base_usecase.dart';
 
+class RemoveMemberUseCase
+    implements UseCase<void, RemoveMemberRequest> {
+  final FamilyRepository _repository;
+
+  const RemoveMemberUseCase({
+    required FamilyRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    RemoveMemberRequest params,
+  ) async {
+    await _repository.removeMember(
+      familyId: params.familyId,
+      userId: params.userId,
+    );
+  }
+}

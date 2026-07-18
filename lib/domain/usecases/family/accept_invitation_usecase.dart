3508403fe4cfca662590fa19ff38a1bafe38f2da
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:26:40 +0500

    Update accept_invitation_usecase.dart

--- lib/domain/usecases/family/accept_invitation_usecase.dart
+++ lib/domain/usecases/family/accept_invitation_usecase.dart
@@ -1,1 +1,22 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/accept_invitation_request.dart';
+import '../../usecases/base_usecase.dart';
 
+/// Accepts a pending family invitation.
+class AcceptInvitationUseCase
+    implements UseCase<void, AcceptInvitationRequest> {
+  final FamilyRepository _repository;
+
+  const AcceptInvitationUseCase({
+    required FamilyRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    AcceptInvitationRequest params,
+  ) async {
+    await _repository.acceptInvitation(
+      invitationId: params.invitationId,
+    );
+  }
+}

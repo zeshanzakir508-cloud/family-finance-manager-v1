54aaff566639dbcc973aedf0bf5bf0b79f5b0ba9
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:42:57 +0500

    Update mark_as_read_request.dart

--- lib/domain/dto/notification/mark_as_read_request.dart
+++ lib/domain/dto/notification/mark_as_read_request.dart
@@ -1,1 +1,8 @@
+/// Request object for marking a notification as read.
+class MarkAsReadRequest {
+  final String notificationId;
 
+  const MarkAsReadRequest({
+    required this.notificationId,
+  });
+}

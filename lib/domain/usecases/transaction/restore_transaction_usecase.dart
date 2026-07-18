b0888a6a10d96fbb3a900038dcc397e5951afbdf
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 14:59:08 +0500

    Update restore_transaction_usecase.dart

--- lib/domain/usecases/transaction/restore_transaction_usecase.dart
+++ lib/domain/usecases/transaction/restore_transaction_usecase.dart
@@ -1,1 +1,32 @@
+import '../../../repositories/transaction_repository.dart';
+import '../../dto/transaction/restore_transaction_request.dart';
+import '../../exceptions/not_found_exception.dart';
+import '../../usecases/base_usecase.dart';
 
+class RestoreTransactionUseCase
+    implements UseCase<void, RestoreTransactionRequest> {
+  final TransactionRepository _repository;
+
+  const RestoreTransactionUseCase({
+    required TransactionRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    RestoreTransactionRequest params,
+  ) async {
+    final exists = await _repository.transactionExists(
+      params.transactionId,
+    );
+
+    if (!exists) {
+      throw const NotFoundException(
+        'Transaction not found.',
+      );
+    }
+
+    await _repository.restoreTransaction(
+      params.transactionId,
+    );
+  }
+}

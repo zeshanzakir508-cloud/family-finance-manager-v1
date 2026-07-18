433d4e9905b2d45c787335525aa5fd3633f1991c
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 19:29:49 +0500

    Update update_settings_usecase.dart

--- lib/domain/usecases/settings/update_settings_usecase.dart
+++ lib/domain/usecases/settings/update_settings_usecase.dart
@@ -1,1 +1,10 @@
+import '../../../models/app_settings_model.dart';
 
+/// Request object for updating application settings.
+class UpdateSettingsRequest {
+  final AppSettingsModel settings;
+
+  const UpdateSettingsRequest({
+    required this.settings,
+  });
+}

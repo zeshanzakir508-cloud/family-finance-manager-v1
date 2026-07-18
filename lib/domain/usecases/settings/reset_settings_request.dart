971ff185c8d0f01b0721db8b9e34ac7e327a1843
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 19:30:47 +0500

    Create reset_settings_request.dart

---
+++ lib/domain/usecases/settings/reset_settings_request.dart
@@ -0,0 +1,8 @@
+/// Request object for resetting application settings.
+class ResetSettingsRequest {
+  final String familyId;
+
+  const ResetSettingsRequest({
+    required this.familyId,
+  });
+}

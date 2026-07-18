f50457c6b78103392e2fb4a5ad2a15db73012bbf
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:06:28 +0500

    Update create_account_usecase.dart

--- lib/domain/usecases/account/create_account_usecase.dart
+++ lib/domain/usecases/account/create_account_usecase.dart
@@ -1,1 +1,23 @@
+import '../../../repositories/account_repository.dart';
+import '../../dto/account/create_account_request.dart';
+import '../../usecases/base_usecase.dart';
+import '../../validators/account_validator.dart';
 
+class CreateAccountUseCase
+    implements UseCase<void, CreateAccountRequest> {
+  final AccountRepository _repository;
+  final AccountValidator _validator;
+
+  const CreateAccountUseCase({
+    required AccountRepository repository,
+    required AccountValidator validator,
+  })  : _repository = repository,
+        _validator = validator;
+
+  @override
+  Future<void> call(CreateAccountRequest params) async {
+    _validator.validate(params);
+
+    await _repository.createAccount(params.account);
+  }
+}

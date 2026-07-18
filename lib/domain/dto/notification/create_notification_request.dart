d858879950d6d1a7f44e5283bfc8cb342c028099
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:41:47 +0500

    Update create_notification_request.dart

--- lib/domain/dto/notification/create_notification_request.dart
+++ lib/domain/dto/notification/create_notification_request.dart
@@ -1,1 +1,10 @@
+import '../../../models/notification_model.dart';
 
+/// Request object for creating a notification.
+class CreateNotificationRequest {
+  final NotificationModel notification;
+
+  const CreateNotificationRequest({
+    required this.notification,
+  });
+}

a7861e54fafdc7728db9c7352437a41b8acbaf17
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:27:04 +0500

    Update reject_invitation_usecase.dart

--- lib/domain/usecases/family/reject_invitation_usecase.dart
+++ lib/domain/usecases/family/reject_invitation_usecase.dart
@@ -1,1 +1,22 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/reject_invitation_request.dart';
+import '../../usecases/base_usecase.dart';
 
+/// Rejects a pending family invitation.
+class RejectInvitationUseCase
+    implements UseCase<void, RejectInvitationRequest> {
+  final FamilyRepository _repository;
+
+  const RejectInvitationUseCase({
+    required FamilyRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    RejectInvitationRequest params,
+  ) async {
+    await _repository.rejectInvitation(
+      invitationId: params.invitationId,
+    );
+  }
+}

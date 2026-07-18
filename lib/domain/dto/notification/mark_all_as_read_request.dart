12ea4942c10f1aab25a1e439908e1532ff846260
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:43:36 +0500

    Update mark_all_as_read_request.dart

--- lib/domain/dto/notification/mark_all_as_read_request.dart
+++ lib/domain/dto/notification/mark_all_as_read_request.dart
@@ -1,1 +1,8 @@
+/// Request object for marking all notifications as read.
+class MarkAllAsReadRequest {
+  final String userId;
 
+  const MarkAllAsReadRequest({
+    required this.userId,
+  });
+}

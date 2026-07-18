a973b08d999b0e141626590376dd09902f9993dc
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:27:28 +0500

    Update cancel_invitation_usecase.dart

--- lib/domain/usecases/family/cancel_invitation_usecase.dart
+++ lib/domain/usecases/family/cancel_invitation_usecase.dart
@@ -1,1 +1,22 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/cancel_invitation_request.dart';
+import '../../usecases/base_usecase.dart';
 
+/// Cancels a pending family invitation.
+class CancelInvitationUseCase
+    implements UseCase<void, CancelInvitationRequest> {
+  final FamilyRepository _repository;
+
+  const CancelInvitationUseCase({
+    required FamilyRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    CancelInvitationRequest params,
+  ) async {
+    await _repository.cancelInvitation(
+      invitationId: params.invitationId,
+    );
+  }
+}

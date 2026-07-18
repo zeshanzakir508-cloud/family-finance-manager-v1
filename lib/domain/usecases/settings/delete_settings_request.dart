1568a9c2815ebdc5277e4b3aa82e7d3473479dc7
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 19:30:16 +0500

    Create delete_settings_request.dart

---
+++ lib/domain/usecases/settings/delete_settings_request.dart
@@ -0,0 +1,8 @@
+/// Request object for deleting application settings.
+class DeleteSettingsRequest {
+  final String familyId;
+
+  const DeleteSettingsRequest({
+    required this.familyId,
+  });
+}

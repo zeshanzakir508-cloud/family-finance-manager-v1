a975644aa2f5863e33e45edc136755924e140856
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:19:35 +0500

    Update create_family_usecase.dart

--- lib/domain/usecases/family/create_family_usecase.dart
+++ lib/domain/usecases/family/create_family_usecase.dart
@@ -1,1 +1,25 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/create_family_request.dart';
+import '../../usecases/base_usecase.dart';
+import '../../validators/family_validator.dart';
 
+class CreateFamilyUseCase
+    implements UseCase<void, CreateFamilyRequest> {
+  final FamilyRepository _repository;
+  final FamilyValidator _validator;
+
+  const CreateFamilyUseCase({
+    required FamilyRepository repository,
+    required FamilyValidator validator,
+  })  : _repository = repository,
+        _validator = validator;
+
+  @override
+  Future<void> call(CreateFamilyRequest params) async {
+    _validator.validate(params);
+
+    await _repository.createFamily(
+      params.family,
+    );
+  }
+}

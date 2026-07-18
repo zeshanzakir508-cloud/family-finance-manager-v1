00505961597e2e6e952f9e5ad2042ff06b883f92
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:22:25 +0500

    Update leave_family_usecase.dart

--- lib/domain/usecases/family/leave_family_usecase.dart
+++ lib/domain/usecases/family/leave_family_usecase.dart
@@ -1,1 +1,22 @@
+import '../../../repositories/family_repository.dart';
+import '../../dto/family/leave_family_request.dart';
+import '../../usecases/base_usecase.dart';
 
+class LeaveFamilyUseCase
+    implements UseCase<void, LeaveFamilyRequest> {
+  final FamilyRepository _repository;
+
+  const LeaveFamilyUseCase({
+    required FamilyRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<void> call(
+    LeaveFamilyRequest params,
+  ) async {
+    await _repository.removeMember(
+      familyId: params.familyId,
+      userId: params.userId,
+    );
+  }
+}

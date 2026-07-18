4bde705993bd0422c97a5f1cceae760874d7c209
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-18 15:43:19 +0500

    Update mark_as_unread_request.dart

--- lib/domain/dto/notification/mark_as_unread_request.dart
+++ lib/domain/dto/notification/mark_as_unread_request.dart
@@ -1,1 +1,8 @@
+/// Request object for marking a notification as unread.
+class MarkAsUnreadRequest {
+  final String notificationId;
 
+  const MarkAsUnreadRequest({
+    required this.notificationId,
+  });
+}

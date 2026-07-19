054b0dcd3860cb9d02f268ab3a4081113475158f
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 19:34:03 +0500

    Create navigation.dart

---
+++ lib/presentation/widgets/navigation/navigation.dart
@@ -0,0 +1,26 @@
+// lib/presentation/widgets/navigation/navigation.dart
+
+/// Navigation library export.
+///
+/// This file exports all navigation widgets and enums for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/navigation/navigation.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Widgets
+// ---------------------------------------------------------------------------
+export 'app_navigation_bar.dart';
+export 'app_navigation_rail.dart';
+export 'app_navigation_drawer.dart';
+export 'app_tab_bar.dart';
+export 'app_breadcrumb.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/navigation_variant.dart';
+export 'enums/navigation_size.dart';
+export 'enums/navigation_label_behavior.dart';

2fab6d445ce397ef31b543f97ebb86563ac1e02e
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 19:30:15 +0500

    Create navigation_label_behavior.dart

---
+++ lib/presentation/widgets/navigation/enums/navigation_label_behavior.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/navigation/enums/navigation_label_behavior.dart
+
+/// The label behavior of the navigation item.
+enum NavigationLabelBehavior {
+  /// Always show labels.
+  alwaysShow,
+
+  /// Show labels when selected only.
+  showSelected,
+
+  /// Never show labels (icons only).
+  neverShow,
+}

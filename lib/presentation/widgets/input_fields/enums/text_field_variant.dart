1465a068b5075a1f3ab3110febe546236e03a7ac
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 14:48:02 +0500

    Create text_field_variant.dart

---
+++ lib/presentation/widgets/input_fields/enums/text_field_variant.dart
@@ -0,0 +1,11 @@
+/// Defines the visual style of an AppTextField.
+enum TextFieldVariant {
+  /// Filled Material text field.
+  filled,
+
+  /// Outlined Material text field.
+  outlined,
+
+  /// Underlined Material text field.
+  underlined,
+}

adf575926df4355d71360c493f0e9dfa7e62970d
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 00:05:53 +0500

    Create chart_label_position.dart

---
+++ lib/presentation/widgets/charts/enums/chart_label_position.dart
@@ -0,0 +1,19 @@
+// lib/presentation/widgets/charts/enums/chart_label_position.dart
+
+/// The position of data labels on charts.
+enum ChartLabelPosition {
+  /// Labels above data points.
+  top,
+
+  /// Labels below data points.
+  bottom,
+
+  /// Labels inside data points.
+  inside,
+
+  /// Labels at the end of bars.
+  end,
+
+  /// No labels displayed.
+  none,
+}

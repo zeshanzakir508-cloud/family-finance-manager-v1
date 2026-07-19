fa351c40feaf4f3c40a357bb797d440e9a8e837b
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 13:22:51 +0500

    Create app_toggle_selection.dart

---
+++ lib/presentation/widgets/buttons/enums/app_toggle_selection.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/buttons/enums/app_toggle_selection.dart
+
+/// The selection behavior of a toggle button group.
+///
+/// Determines whether multiple options can be selected
+/// or only a single option at a time.
+enum AppToggleSelection {
+  /// Only one option can be selected at a time.
+  single,
+
+  /// Multiple options can be selected simultaneously.
+  multiple,
+}

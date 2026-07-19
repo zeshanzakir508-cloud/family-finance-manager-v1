05c3b96346ca1164b0225f4b2f0b76cb22f81e99
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 16:50:59 +0500

    Create chip_size.dart

---
+++ lib/presentation/widgets/chips/enums/chip_size.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/chips/enums/chip_size.dart
+
+/// The size of the chip.
+enum ChipSize {
+  /// Small chip (24px height).
+  small,
+
+  /// Medium chip (32px height).
+  medium,
+
+  /// Large chip (40px height).
+  large,
+}

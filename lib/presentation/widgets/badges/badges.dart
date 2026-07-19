704d5f36e68ce0f91bd9732fa052a552deeb924a
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 19:44:41 +0500

    Create badges.dart

---
+++ lib/presentation/widgets/badges/badges.dart
@@ -0,0 +1,25 @@
+// lib/presentation/widgets/badges/badges.dart
+
+/// Badges library export.
+///
+/// This file exports all badge widgets and enums for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/badges/badges.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Widgets
+// ---------------------------------------------------------------------------
+export 'app_badge.dart';
+export 'app_status_badge.dart';
+export 'app_count_badge.dart';
+export 'app_dot_badge.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/badge_variant.dart';
+export 'enums/badge_size.dart';
+export 'enums/badge_shape.dart';

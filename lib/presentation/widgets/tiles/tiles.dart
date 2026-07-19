53060ad0244fd25d2cb3947d695d2ffae3427e5b
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 17:11:17 +0500

    Create tiles.dart

---
+++ lib/presentation/widgets/tiles/tiles.dart
@@ -0,0 +1,27 @@
+// lib/presentation/widgets/tiles/tiles.dart
+
+/// Tiles library export.
+///
+/// This file exports all tile widgets and enums for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/tiles/tiles.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Widgets
+// ---------------------------------------------------------------------------
+export 'app_tile.dart';
+export 'app_navigation_tile.dart';
+export 'app_setting_tile.dart';
+export 'app_transaction_tile.dart';
+export 'app_account_tile.dart';
+export 'app_member_tile.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/tile_variant.dart';
+export 'enums/tile_size.dart';
+export 'enums/tile_density.dart';

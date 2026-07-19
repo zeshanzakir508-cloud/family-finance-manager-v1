564d91f2185a42e0c02d55c3e23b030c3a1dcfc8
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 19:47:45 +0500

    Create avatar_shape.dart

---
+++ lib/presentation/widgets/avatars/enums/avatar_shape.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/avatars/enums/avatar_shape.dart
+
+/// The shape of the avatar.
+enum AvatarShape {
+  /// Circular avatar.
+  circular,
+
+  /// Rounded rectangular avatar.
+  rounded,
+
+  /// Square avatar.
+  square,
+}

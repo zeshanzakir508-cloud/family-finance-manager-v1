59f9bc30b7d013642946f9519025fcd34d415541
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 00:56:00 +0500

    Create media.dart

---
+++ lib/presentation/widgets/media/media.dart
@@ -0,0 +1,31 @@
+// lib/presentation/widgets/media/media.dart
+
+/// Media library export.
+///
+/// This file exports all media widgets and enums for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/media/media.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Widgets
+// ---------------------------------------------------------------------------
+export 'app_image.dart';
+export 'app_network_image.dart';
+export 'app_asset_image.dart';
+export 'app_svg.dart';
+export 'app_image_viewer.dart';
+export 'app_zoomable_image.dart';
+export 'app_image_gallery.dart';
+export 'app_blurred_image.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/image_fit.dart';
+export 'enums/image_shape.dart';
+export 'enums/image_source.dart';
+export 'enums/image_placeholder.dart';
+export 'enums/image_quality.dart';

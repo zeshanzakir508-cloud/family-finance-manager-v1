8267fa842003d811306468afec7479ee389a8bd8
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 00:04:56 +0500

    Create chart_legend_position.dart

---
+++ lib/presentation/widgets/charts/enums/chart_legend_position.dart
@@ -0,0 +1,22 @@
+// lib/presentation/widgets/charts/enums/chart_legend_position.dart
+
+/// The position of the chart legend.
+enum ChartLegendPosition {
+  /// Legend above the chart.
+  top,
+
+  /// Legend below the chart.
+  bottom,
+
+  /// Legend to the left of the chart.
+  left,
+
+  /// Legend to the right of the chart.
+  right,
+
+  /// Legend overlays the chart.
+  overlay,
+
+  /// No legend displayed.
+  none,
+}

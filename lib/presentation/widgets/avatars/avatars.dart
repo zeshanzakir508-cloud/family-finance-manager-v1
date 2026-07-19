2cbf96071cd8470ff79df757a9fec2e484272038
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 19:50:31 +0500

    Create avatars.dart

---
+++ lib/presentation/widgets/avatars/avatars.dart
@@ -0,0 +1,25 @@
+// lib/presentation/widgets/avatars/avatars.dart
+
+/// Avatars library export.
+///
+/// This file exports all avatar widgets and enums for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/avatars/avatars.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Widgets
+// ---------------------------------------------------------------------------
+export 'app_avatar.dart';
+export 'app_initials_avatar.dart';
+export 'app_network_avatar.dart';
+export 'app_group_avatar.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/avatar_variant.dart';
+export 'enums/avatar_size.dart';
+export 'enums/avatar_shape.dart';

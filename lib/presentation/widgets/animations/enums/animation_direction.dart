8e6989512243e94e03ead50328ea4c3839b54021
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 00:39:29 +0500

    Create animation_direction.dart

---
+++ lib/presentation/widgets/animations/enums/animation_direction.dart
@@ -0,0 +1,22 @@
+// lib/presentation/widgets/animations/enums/animation_direction.dart
+
+/// The direction of the animation.
+enum AnimationDirection {
+  /// From top to bottom.
+  fromTop,
+
+  /// From bottom to top.
+  fromBottom,
+
+  /// From left to right.
+  fromLeft,
+
+  /// From right to left.
+  fromRight,
+
+  /// From center outward.
+  fromCenter,
+
+  /// From outward to center.
+  toCenter,
+}

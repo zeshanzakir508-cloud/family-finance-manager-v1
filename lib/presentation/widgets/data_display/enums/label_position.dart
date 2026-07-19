1c6dbc04e57464b2041cdb201a1666563bd33476
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 23:48:29 +0500

    Create label_position.dart

---
+++ lib/presentation/widgets/data_display/enums/label_position.dart
@@ -0,0 +1,16 @@
+// lib/presentation/widgets/data_display/enums/label_position.dart
+
+/// The position of the label relative to the value.
+enum LabelPosition {
+  /// Label above the value.
+  top,
+
+  /// Label left of the value.
+  left,
+
+  /// Label right of the value.
+  right,
+
+  /// Label below the value.
+  bottom,
+}

7b9c8f6d9bc7c422144ac0c57814607dbc4cecf4
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 19:40:48 +0500

    Create badge_size.dart

---
+++ lib/presentation/widgets/badges/enums/badge_size.dart
@@ -0,0 +1,19 @@
+// lib/presentation/widgets/badges/enums/badge_size.dart
+
+/// The size of the badge.
+enum BadgeSize {
+  /// Extra small badge (16px).
+  extraSmall,
+
+  /// Small badge (20px).
+  small,
+
+  /// Medium badge (24px).
+  medium,
+
+  /// Large badge (28px).
+  large,
+
+  /// Extra large badge (32px).
+  extraLarge;
+}

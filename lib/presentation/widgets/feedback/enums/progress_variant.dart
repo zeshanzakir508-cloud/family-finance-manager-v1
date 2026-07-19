f1039c9759191702168c4bb436a176a6eac25de3
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 23:29:14 +0500

    Create progress_variant.dart

---
+++ lib/presentation/widgets/feedback/enums/progress_variant.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/feedback/enums/progress_variant.dart
+
+/// The visual variant of the progress indicator.
+enum ProgressVariant {
+  /// Linear progress indicator.
+  linear,
+
+  /// Circular progress indicator.
+  circular,
+
+  /// Segmented progress indicator (for steps).
+  segmented,
+}

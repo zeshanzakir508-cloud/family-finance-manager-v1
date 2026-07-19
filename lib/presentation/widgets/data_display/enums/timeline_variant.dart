33595e9f407f2cf9ec4b0c72008db55ca539f6ea
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 23:47:57 +0500

    Create timeline_variant.dart

---
+++ lib/presentation/widgets/data_display/enums/timeline_variant.dart
@@ -0,0 +1,10 @@
+// lib/presentation/widgets/data_display/enums/timeline_variant.dart
+
+/// The visual variant of the timeline.
+enum TimelineVariant {
+  /// Vertical timeline.
+  vertical,
+
+  /// Horizontal timeline.
+  horizontal,
+}

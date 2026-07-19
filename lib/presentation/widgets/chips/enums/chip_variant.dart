5229eec4ebd48cd2f52a89ec021e03be64473119
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 16:50:41 +0500

    Create chip_variant.dart

---
+++ lib/presentation/widgets/chips/enums/chip_variant.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/chips/enums/chip_variant.dart
+
+/// The visual variant of the chip.
+enum ChipVariant {
+  /// Filled chip with background color.
+  filled,
+
+  /// Outlined chip with border.
+  outlined,
+
+  /// Tonal chip with tonal background color.
+  tonal,
+}

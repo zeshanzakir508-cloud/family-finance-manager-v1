5eff71f70bdfd7eab9f6a801b04b262e53a7f36f
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 14:57:17 +0500

    Create input_fields.dart

---
+++ lib/presentation/widgets/input_fields/input_fields.dart
@@ -0,0 +1,53 @@
+/// Input Fields library export.
+///
+/// Import this file to access all input field widgets,
+/// enums, builders, helpers, and constants.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/input_fields/input_fields.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Widgets
+// ---------------------------------------------------------------------------
+export 'app_text_field.dart';
+export 'app_search_field.dart';
+export 'app_password_field.dart';
+export 'app_number_field.dart';
+export 'app_amount_field.dart';
+export 'app_otp_field.dart';
+export 'app_dropdown.dart';
+export 'app_multiselect.dart';
+export 'app_checkbox.dart';
+export 'app_radio.dart';
+export 'app_slider.dart';
+export 'app_range_slider.dart';
+export 'app_chip.dart';
+export 'app_autocomplete.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/text_field_variant.dart';
+export 'enums/text_field_size.dart';
+export 'enums/text_field_state.dart';
+
+// ---------------------------------------------------------------------------
+// Constants
+// ---------------------------------------------------------------------------
+export 'constants/text_field_constants.dart';
+
+// ---------------------------------------------------------------------------
+// Helpers
+// ---------------------------------------------------------------------------
+export 'helpers/text_field_border.dart';
+export 'helpers/text_field_dimensions.dart';
+export 'helpers/text_field_padding.dart';
+export 'helpers/text_field_radius.dart';
+export 'helpers/text_field_text_style.dart';
+
+// ---------------------------------------------------------------------------
+// Builders
+// ---------------------------------------------------------------------------
+export 'builders/text_field_decoration_builder.dart';

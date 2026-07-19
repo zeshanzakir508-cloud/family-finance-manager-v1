3c43abe4a583353fd3852e6329154985fa20a87d
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 00:49:04 +0500

    Create image_shape.dart

---
+++ lib/presentation/widgets/media/enums/image_shape.dart
@@ -0,0 +1,16 @@
+// lib/presentation/widgets/media/enums/image_shape.dart
+
+/// The shape of the image container.
+enum ImageShape {
+  /// Square/rectangular image.
+  rectangle,
+
+  /// Rounded rectangle image.
+  rounded,
+
+  /// Circular image.
+  circular,
+
+  /// Square image.
+  square,
+}

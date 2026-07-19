9cac821e694eceb6c1fc0028bcc58a1cd4e77572
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 23:56:59 +0500

    Create selection_state.dart

---
+++ lib/presentation/widgets/selection/enums/selection_state.dart
@@ -0,0 +1,16 @@
+// lib/presentation/widgets/selection/enums/selection_state.dart
+
+/// The state of the selection widget.
+enum SelectionState {
+  /// Unselected state.
+  unselected,
+
+  /// Selected state.
+  selected,
+
+  /// Indeterminate state (partial selection).
+  indeterminate,
+
+  /// Disabled state.
+  disabled,
+}

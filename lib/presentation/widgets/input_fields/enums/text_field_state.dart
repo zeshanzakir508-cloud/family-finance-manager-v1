cdb897ecd49e982308a4fd3bac42df322b2a1b51
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 14:49:54 +0500

    Create text_field_state.dart

---
+++ lib/presentation/widgets/input_fields/enums/text_field_state.dart
@@ -0,0 +1,14 @@
+/// Defines the current visual state of an AppTextField.
+enum TextFieldState {
+  /// Default state.
+  normal,
+
+  /// Indicates successful validation.
+  success,
+
+  /// Indicates validation error.
+  error,
+
+  /// Indicates a warning.
+  warning,
+}

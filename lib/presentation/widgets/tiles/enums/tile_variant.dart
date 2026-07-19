006f57a3854d3ee02226e75cd057d41c6e9cff47
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 17:07:40 +0500

    Create tile_variant.dart

---
+++ lib/presentation/widgets/tiles/enums/tile_variant.dart
@@ -0,0 +1,13 @@
+// lib/presentation/widgets/tiles/enums/tile_variant.dart
+
+/// The visual variant of the tile.
+enum TileVariant {
+  /// Filled tile with background color.
+  filled,
+
+  /// Outlined tile with border.
+  outlined,
+
+  /// Tonal tile with tonal background color.
+  tonal,
+}

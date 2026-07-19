728326f1d4c02ebc34f2a18c7fed9dd730a99207
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-19 15:44:16 +0500

    Update dialog_icon_builder.dart

--- lib/presentation/widgets/dialogs/helpers/dialog_icon_builder.dart
+++ lib/presentation/widgets/dialogs/helpers/dialog_icon_builder.dart
@@ -1,31 +1,29 @@
+// lib/presentation/widgets/dialogs/helpers/dialog_icon_builder.dart
+
 import 'package:flutter/material.dart';
 
 import '../enums/dialog_type.dart';
 
-/// Builds the appropriate icon for a dialog based on its [DialogType].
+/// Helper class for mapping [DialogType] to [IconData].
 ///
-/// This helper centralizes dialog icon selection to keep dialog widgets
-/// clean and consistent throughout the application.
-final class DialogIconBuilder {
-  const DialogIconBuilder._();
-
-  /// Returns the icon corresponding to the given [DialogType].
+/// Example:
+/// ```dart
+/// final iconData = DialogIconBuilder.build(DialogType.success);
+/// ```
+abstract final class DialogIconBuilder {
+  /// Returns the appropriate [IconData] for the given [type].
   static IconData build(DialogType type) {
     switch (type) {
-      case DialogType.info:
-        return Icons.info_outline;
-
       case DialogType.success:
-        return Icons.check_circle_outline;
-
+        return Icons.check_circle;
+      case DialogType.info:
+        return Icons.info;
       case DialogType.warning:
-        return Icons.warning_amber_rounded;
-
+        return Icons.warning;
       case DialogType.error:
-        return Icons.error_outline;
-
+        return Icons.error;
       case DialogType.confirmation:
-        return Icons.help_outline;
+        return Icons.help;
     }
   }
 }

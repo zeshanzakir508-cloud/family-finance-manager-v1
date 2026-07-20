e793c6a8668be4ba85562ddad99e6c519a9a6a29
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 17:43:31 +0500

    Create lifecycle_state.dart

---
+++ lib/presentation/widgets/utility/enums/lifecycle_state.dart
@@ -0,0 +1,16 @@
+// lib/presentation/widgets/utility/enums/lifecycle_state.dart`
+
+/// The lifecycle state of the application.
+enum LifecycleState {
+  /// The application is resumed and visible.
+  resumed,
+
+  /// The application is in the background.
+  inactive,
+
+  /// The application is paused.
+  paused,
+
+  /// The application is detached.
+  detached,
+}

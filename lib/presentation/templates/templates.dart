03cebf8006df5055935ed95d4a9de2a334a9e8ec
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 18:19:24 +0500

    Create templates.dart

---
+++ lib/presentation/templates/templates.dart
@@ -0,0 +1,30 @@
+// lib/presentation/templates/templates.dart
+
+/// Templates library export.
+///
+/// This file exports all page templates for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/templates/templates.dart';
+/// ```
+
+// ---------------------------------------------------------------------------
+// Main Templates
+// ---------------------------------------------------------------------------
+export 'app_page.dart';
+export 'app_list_page.dart';
+export 'app_form_page.dart';
+export 'app_dashboard_page.dart';
+export 'app_detail_page.dart';
+export 'app_settings_page.dart';
+export 'app_master_detail_page.dart';
+export 'app_tab_page.dart';
+export 'app_split_view.dart';
+
+// ---------------------------------------------------------------------------
+// Enums
+// ---------------------------------------------------------------------------
+export 'enums/page_density.dart';
+export 'enums/page_scroll_behavior.dart';
+export 'enums/page_variant.dart';

af5928894424c6c31c676195cefd8a9795330eb0
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 18:01:27 +0500

    Create transactions.dart

---
+++ lib/presentation/widgets/domain/transactions/transactions.dart
@@ -0,0 +1,16 @@
+// lib/presentation/widgets/domain/transactions/transactions.dart
+
+/// Transactions domain library export.
+///
+/// This file exports all transaction-related domain widgets for convenient importing.
+///
+/// Example:
+/// ```dart
+/// import 'package:family_finance/presentation/widgets/domain/transactions/transactions.dart';
+/// ```
+
+export 'transaction_card.dart';
+export 'transaction_tile.dart';
+export 'transaction_amount.dart';
+export 'transaction_status_badge.dart';
+export 'transaction_timeline.dart';

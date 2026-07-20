63197cd4a4acb48f1ec4d5dd15d840d987f68cf3
zeshanzakir508-cloud <zeshanzakir508@gmail.com>
2026-07-20 17:42:51 +0500

    Create platform_type.dart

---
+++ lib/presentation/widgets/utility/enums/platform_type.dart
@@ -0,0 +1,28 @@
+// lib/presentation/widgets/utility/enums/platform_type.dart
+
+/// The type of platform running the application.
+enum PlatformType {
+  /// Android platform.
+  android,
+
+  /// iOS platform.
+  ios,
+
+  /// Windows platform.
+  windows,
+
+  /// Linux platform.
+  linux,
+
+  /// macOS platform.
+  macos,
+
+  /// Web platform.
+  web,
+
+  /// Fuchsia platform.
+  fuchsia,
+
+  /// Unknown platform.
+  unknown,
+}
